ae3924ac3718cae2abdc86180f85fc3dad31e871
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-10 14:07:27 +0300

    add task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 if __name__ == "__main__":
-    from add_0 import add
-
-    a = 1
-    b = 2
-
-    print("{} + {} = {}".format(a, b, add(a,b)))
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
