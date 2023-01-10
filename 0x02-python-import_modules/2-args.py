28fe23b63e5b1c2b1b6e5ee972ce397b6017dc35
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-10 14:38:37 +0300

    add task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
         i = 0
         for arg in sys.argv:
             if i != 0:
-                print("{} : {}".format(i, arg))
+                print("{}: {}".format(i, arg))
             i += 1
