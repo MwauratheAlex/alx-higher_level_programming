c56caf77931bfc4a767e5770bce1cd33c65d7fb7
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-11 09:25:02 +0300

    edit task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,9 +3,9 @@
 def no_c(my_string):
     str_list = list(my_string)
 
-    if 'c' in str_list:
+    while 'c' in str_list:
         str_list.remove('c')
-    if 'C' in str_list:
+    while 'C' in str_list:
         str_list.remove('C')
 
     return ''.join(str_list)
