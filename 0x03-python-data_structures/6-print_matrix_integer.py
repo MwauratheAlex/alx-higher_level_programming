6d1f7b77d39d39c7671d16aa8ea0be17bb67c061
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-11 09:40:05 +0300

    edit task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix == None:
+    if matrix is None:
         return
     for item in matrix:
         for i in range(len(item)):
