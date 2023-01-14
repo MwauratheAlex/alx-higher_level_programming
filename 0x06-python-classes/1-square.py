f938cc0b50d2886bd1600bd8c9c2b8a825000902
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-14 14:37:06 +0300

    add task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Module containing a class defining a square"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size):
+        self.__size = size
