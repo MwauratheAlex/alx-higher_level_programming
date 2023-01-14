4252732673afd440b69e14bebf7828c24724175a
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-14 15:12:44 +0300

    add task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Module contains a class that defines a square"""
+
+
+class Square:
+    """Class defines a square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__init__(value)
+
+    def area(self):
+        return self.__size * self.__size
