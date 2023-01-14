a882f004f1462e0b19ff234abf428ded28de4bac
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-14 16:20:39 +0300

    add task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,6 +42,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            for i in range(self.__position[1]):
+                print()
             for i in range(self.__size):
                 for i in range(self.__position[0]):
                     print(" ", end="")
