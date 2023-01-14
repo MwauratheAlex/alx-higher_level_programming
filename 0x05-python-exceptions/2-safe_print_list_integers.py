8c6fef97dc10cfc577e3f6ac7148a9fd251b847b
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-14 09:04:32 +0300

    add task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    i = count = 0
+    while True:
+        try:
+            if i < x:
+                print("{:d}".format(my_list[i]), end="")
+                i += 1
+                count += 1
+            else:
+                print()
+                return count
+        except(ValueError, TypeError):
+            i += 1
