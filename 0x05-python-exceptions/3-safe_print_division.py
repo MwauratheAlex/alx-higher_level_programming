8bfb6dfd0204ad57ad15a5b90f308ff8ab72de90
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-14 09:09:11 +0300

    add task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except Exception:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
