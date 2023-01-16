ead18f17405601d865cc5171285a7caa76231fda
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-16 17:12:52 +0300

    add task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Module contains one function, ``print_square(size)``
+"""
+
+
+def print_square(size):
+    """The function prints a square of size = size"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    print(('#' * size + "\n") * size, end="")
