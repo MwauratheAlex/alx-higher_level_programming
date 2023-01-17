91e0a0bb752be3fa89f9645450276100f4a26253
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-17 12:10:50 +0300

    add task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Module contains a class MyList that inherits from the class list
+The class contains a pulic instance function ``print_sorted(self)``
+The function prints a sorted list in ascending order
+"""
+
+
+class MyList(list):
+    """
+    Inherits from the list class
+    Contains a function print_sorted(self)
+    The function prints a sorted list in ascending order
+    """
+
+    def __init__(self):
+        super().__init__(self)
+
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        self.sort()
+        print(self)
