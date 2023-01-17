73073b9d64a95adb5e2449a24a0836c09c85d192
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-17 09:06:15 +0300

    add task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -23,5 +23,25 @@ class TestMaxInteger(unittest.TestCase):
         """Test if no arguement is included"""
         self.assertIsNone(max_integer())
 
+    def test_max_at_end(self):
+        """Test for max int at end"""
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+
+    def test_max_at_beginning(self):
+        """Test for max at beginning"""
+        self.assertEqual(max_integer([3, 2, 1]), 3)
+
+    def test_max_at_middle(self):
+        """Test for max in the middle"""
+        self.assertEqual(max_integer([1, 3, 2]), 3)
+
+    def test_one_negative(self):
+        """Test if the list contains one negative number"""
+        self.assertEqual(max_integer([1, -3, 2],), 2)
+
+    def test_one_element_list(self):
+        """Test if the list has one element"""
+        self.assertEqual(max_integer([3]), 3)
+
 if __name__ == "__main__":
     unittest.main()
