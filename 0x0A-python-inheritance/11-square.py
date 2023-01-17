9b6aae477d51a524a1b0dbea77945a06853a0b6f
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-17 18:31:15 +0300

    add task 10

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+Module contains the Square class
+that inherits from the Rectangle class
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class defines a rectangle and
+    inherits from the rectangle class
+    Class contains the method area that
+    calculates the area of the square
+
+    and the method __str__ that prints the string 
+    representation of the square
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Prints the screen representation of the square"""
+        return super().__str__()
