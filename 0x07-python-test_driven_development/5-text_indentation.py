4587398760692069f619b758447e14ab2375bd66
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-17 07:35:53 +0300

    add task 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+This module contains one function ``text_indentation(text)``
+"""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines after each of these characters:
+    ., ? and :"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    flag = 0
+    for ch in text:
+        if ch not in ".?:":
+            if flag == 1:
+                flag = 0
+                continue
+            print(ch, end="")
+        else:
+            print("\n")
+            flag = 1
