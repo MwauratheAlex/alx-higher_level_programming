168f61f455433090b3cea903e32a8e890542c8b2
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-18 11:08:12 +0300

    add task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+This module contains the class Student
+"""
+
+
+class Student:
+    """This class defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+        return vars(self)
