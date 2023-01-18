e5095d2351b59bf3d20c26d2effd50e725121585
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-18 16:59:50 +0300

    add task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+This module contains the class Student
+"""
+
+
+class Student:
+    """This class defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return vars(self)
+        class_dict_rtn = {}
+        for item in attrs:
+            if item in vars(self):
+                class_dict_rtn[item] = vars(self)[item]
+        return class_dict_rtn
