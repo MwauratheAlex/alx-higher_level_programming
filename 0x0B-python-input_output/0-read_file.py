b834f8861e9e443dc01bfaa04dfff315668c0db9
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-18 09:41:48 +0300

    add task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+This module contains the function ``read_file(filename="")``
+The function  reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def  read_file(filename=""):
+    """ reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
