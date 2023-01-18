c8b8a421e935b28e573f264e6883d325a6f6c00e
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-18 09:43:03 +0300

    correct multiple spaces after keyword

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,7 @@ The function  reads a text file (UTF8) and prints it to stdout
 """
 
 
-def  read_file(filename=""):
+def read_file(filename=""):
     """ reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
         for line in f:
