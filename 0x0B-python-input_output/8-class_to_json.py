b2aa150a56102dbec09742bc62cc43acdea37de2
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-18 11:02:41 +0300

    add task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+This module contains the function ``class_to_json(obj)``
+that returns the dictionary description with simple
+data structure (list, dictionary, string, integer and boolean)
+for JSON serialization of an object
+"""
+
+import json
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+    """
+    return vars(obj)
