d8645bb64df686bb2ecb84435ae10db39b7a6c9c
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-21 08:31:47 +0300

    add task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,2 +1,32 @@
 #!/usr/bin/python3
+"""
+This module contains the class Student
+"""
 
+
+class Student:
+    """This class defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return vars(self)
+        class_dict_rtn = {}
+        for item in attrs:
+            if item in vars(self):
+                class_dict_rtn[item] = vars(self)[item]
+        return class_dict_rtn
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            if key == "first_name":
+                self.first_name = value
+            if key == "last_name":
+                self.last_name = value
+            if key == "age":
+                self.age = value
