47399d56aaefbc965f8bd7a716966e21ba09d9c2
MwauratheAlex <mwaurathealex@gmail.com>
2023-02-15 19:59:32 +0300

    add task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS cities (
     `id`       INT          NOT NULL AUTO_INCREMENT,
     `state_id` INT          NOT NULL,
     `name`     VARCHAR(256) NOT NULL,
-    FOREIGN KEY(`state_id`)
-    REFERENCES `hbtn_0d_usa`.`states`(`id`)
+    FOREIGN KEY(state_id)
+    REFERENCES states(id)
 );
