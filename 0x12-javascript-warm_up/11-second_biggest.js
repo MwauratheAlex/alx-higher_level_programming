ec672dd050538400bc5ed56b32c9c9025498c5dc
MwauratheAlex <mwaurathealex@gmail.com>
2023-03-13 15:52:32 +0300

    add task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+function secondLargest (args) {
+  let largest = 0;
+  let secondLargest = 0;
+
+  for (let i = 0; i < args.length; i++) {
+    const num = parseInt(args[i]);
+
+    if (isNaN(num)) {
+      return (0);
+    }
+
+    if (num > largest) {
+      secondLargest = largest;
+      largest = num;
+    }
+  }
+  return (secondLargest);
+}
+
+console.log(secondLargest(args));
