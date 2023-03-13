bfcde7321c9b2dd6dc5b42131bed1097be823271
MwauratheAlex <mwaurathealex@gmail.com>
2023-03-13 15:33:56 +0300

    add task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const n = parseInt(process.argv[2]);
+
+function factorial (n) {
+  if (isNaN(n)) {
+    return (1);
+  }
+
+  if (n === 0) {
+    return (1);
+  }
+
+  return (n * factorial(n - 1));
+}
+
+console.log(factorial(n));
