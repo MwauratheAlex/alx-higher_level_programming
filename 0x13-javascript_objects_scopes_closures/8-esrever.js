4251d48a00aa203be3a5c2891a4e53cea88a04a8
MwauratheAlex <mwaurathealex@gmail.com>
2023-03-14 14:13:47 +0300

    add task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+exports.esrever = function (list) {
+  const revList = [];
+  let j = 0;
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    revList[j] = list[i];
+    j++;
+  }
+
+  return (revList);
+};
