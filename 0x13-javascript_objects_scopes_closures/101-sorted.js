3e32ecd215cdb46db61e0c5019e5ab68f2fb3a20
MwauratheAlex <mwaurathealex@gmail.com>
2023-03-14 17:29:47 +0300

    add task 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,6 +3,7 @@
 const dict = require('./101-data').dict;
 const newDict = {};
 
+/*
 Object.entries(dict).forEach(([key, value]) => {
   if (!newDict[value]) {
     newDict[value] = [key];
@@ -10,5 +11,15 @@ Object.entries(dict).forEach(([key, value]) => {
     newDict[value].push(key);
   }
 });
+*/
+
+for (const key of Object.keys(dict)) {
+  const value = dict[key];
+  if (!newDict[value]) {
+    newDict[value] = [key];
+  } else {
+    newDict[value].push(key);
+  }
+}
 
 console.log(newDict);
