d10a45bf47ea9befcf36a467e098f439af71c293
MwauratheAlex <mwaurathealex@gmail.com>
2023-03-23 13:31:50 +0300

    add task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""prints all City objects from the database hbtn_0e_14_usa.
+Args:
+    mysql username, mysql password and database name"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from urllib.parse import quote_plus
+    from model_state import Base, State
+    from model_city import City
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}"
+            .format(argv[1], quote_plus(argv[2]), argv[3]),
+            pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.\
+        query(State, City).\
+        filter(State.id == City.state_id).\
+        order_by(City.id.asc()).\
+        all()
+
+    for state, city in result:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""contains the class definition of a City"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """Describes a state"""
+    __tablename__ = "cities"
+    id = Column(Integer,
+                autoincrement=True,
+                unique=True,
+                nullable=False,
+                primary_key=True)
+    name = Column(String(128),  nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
