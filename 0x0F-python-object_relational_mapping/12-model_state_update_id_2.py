bbdd6c67277357c4f0f4f0b0a97a4a5c031d0d59
MwauratheAlex <mwaurathealex@gmail.com>
2023-03-23 12:00:14 +0300

    add task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""The script changes the name of a State object
+from the database hbtn_0e_6_usa.
+Args:
+    mysql username, mysql password and database name"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from urllib.parse import quote_plus
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}"
+            .format(argv[1], quote_plus(argv[2]), argv[3]),
+            pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+
+    if state:
+        state.name = "New Mexico"
+
+    session.commit()
+    session.close()
