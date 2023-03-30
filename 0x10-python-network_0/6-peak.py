9cf53b377761d2232c2a5ede8944120095096c18
MwauratheAlex <mwaurathealex@gmail.com>
2023-03-30 12:57:17 +0300

    add task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,8 +2,10 @@
 """This module contains the function find_peak"""
 
 def find_peak(list_of_integers):
+    """This funtion finds a peak in a list of unsorted integers"""
     if list_of_integers is None or len(list_of_integers) == 0:
         return None
     list_of_integers.sort(reverse=True)
     print(list_of_integers)
     return list_of_integers[0]
+
