443f5ddb1ca53c58b475bffab8460bc35debbd87
MwauratheAlex <mwaurathealex@gmail.com>
2023-03-30 12:07:04 +0300

    add task 3

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #takes in a URL, sends a GET request to the URL, and displays the body of the response
-response=$(curl -s -o /dev/null -w "%{http_code}" $1)
+response=$(curl -X GET -s -o /dev/null -w "%{http_code}" $1)
 
 if [ $response -eq 200 ]; then
 	curl $1
