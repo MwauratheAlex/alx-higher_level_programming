92a989b6b4ab9073712a0cdf1792e1d259b47a67
MwauratheAlex <mwaurathealex@gmail.com>
2023-04-02 13:25:36 +0300

    add task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""takes in a URL, sends a request to the URL and displays the value
+of the X-Request-Id variable found in the header of the response."""
+import sys
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    print(response.headers.__getitem__('X-Request-Id'))
