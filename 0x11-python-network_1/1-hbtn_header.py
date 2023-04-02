bc62e1abfa0561b3f0b1eb3afb7eb7594463bce1
MwauratheAlex <mwaurathealex@gmail.com>
2023-04-02 13:30:42 +0300

    add task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the URL and displays the value
-of the X-Request-Id variable found in the header of the response."""
-
 import sys
 import urllib.request
 
