761b1e2aa64db555ff606c2fab32e5ba14bee2f4
MwauratheAlex <mwaurathealex@gmail.com>
2023-04-02 16:42:14 +0300

    add task 3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""takes in a URL, sends a request to the URL and displays the body of the response"""
+
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+
+    url = sys.argv[1]
+    print(url)
+
+    with urllib.request.urlopen(url) as response:
+        print(response.read())
