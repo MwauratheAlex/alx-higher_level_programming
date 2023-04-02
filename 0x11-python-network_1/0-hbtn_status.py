dc7894767c794c3b50f4b20d0bc68c2b06b289d3
MwauratheAlex <mwaurathealex@gmail.com>
2023-04-02 13:14:25 +0300

    add task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    f = response.read()
+    print('Body response:')
+    print('\t- type:', type(f))
+    print('\t- content:', f)
+    print('\t- utf8 content:', f.decode('utf-8'))
