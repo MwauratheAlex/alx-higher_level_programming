0b5452e6eb07583ba7440b36206364e84766b8d9
MwauratheAlex <mwaurathealex@gmail.com>
2023-04-02 13:39:41 +0300

    Your code should not be executed when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    f = response.read()
-    print('Body response:')
-    print('\t- type:', type(f))
-    print('\t- content:', f)
-    print('\t- utf8 content:', f.decode('utf-8'))
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        f = response.read()
+        print('Body response:')
+        print('\t- type:', type(f))
+        print('\t- content:', f)
+        print('\t- utf8 content:', f.decode('utf-8'))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
-import sys
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    print(response.headers.__getitem__('X-Request-Id'))
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.__getitem__('X-Request-Id'))
