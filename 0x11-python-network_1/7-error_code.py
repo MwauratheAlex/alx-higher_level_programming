627f2a7830c4907e557669fe096af57194254f23
MwauratheAlex <mwaurathealex@gmail.com>
2023-04-09 16:52:08 +0300

    add task 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""takes in a URL, sends a request to the URL and
+displays the body of the response."""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+
+    with requests.get(url) as response:
+        status_code = response.status_code
+
+        if status_code >= 400:
+            print("Error code: {}".format(status_code))
+        else:
+            print(response.text)
