a0eb48645ec7756dfe03a6c2ea2b0e6bc8f9c52a
MwauratheAlex <mwaurathealex@gmail.com>
2023-04-09 16:11:11 +0300

    add 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    import requests
+
+    url = "https://alx-intranet.hbtn.io/status"
+    print("Body response:")
+
+    with requests.get(url) as response:
+        r = response.text
+        print("\t- type: {}".format(type(r)))
+        print("\t- content: {}".format(r))
